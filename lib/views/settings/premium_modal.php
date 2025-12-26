5c9aaddda752ec42b4e42057edf5bb22e520a851
halpha-dev <hadisajjadi777@gmail.com>
2025-12-26 20:35:50 +0330

    Add files via upload

---
+++ lib/views/settings/premium_modal.php
@@ -0,0 +1,181 @@
+<div class="latepoint-lightbox-heading">
+	<h2><?php esc_html_e('Unlock Premium Features', 'latepoint'); ?></h2>
+</div>
+<div class="latepoint-lightbox-content">
+<div class="os-premium-features-list">
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-browser"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Custom Fields', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Create additional fields to capture data from customers. File upload, address autocomplete, text, checkbox and more', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-refresh"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Recurring Appointments', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Native support for WordPress blocks, Elementor, Bricks builder, or simply use a shortcode.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-clock"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Activity Log', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Our built in activity log will help you trace everything that is happening with your booking business.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-map-marker"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Google Address Autocomplete', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e("Capture accurate address information by utilizing Google's autocomplete address field.", 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-users1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Dashboard for Agents', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Let your agents access their own dashboard, where they can manage appointments, customers, and their work schedule.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-user1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Customer Cabinet', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Let your customers access their personal cabinet, which allows them to cancel and review their appointments and personal information.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-calendar1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Google Calendar Sync', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Connect your Google Calendar to automatically sync your availability.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-video-camera"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Zoom & Google Meet', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Automatically create zoom and Google Meet video meetings for each appointment.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-bubble"></i></div>
+    <div class="feature-heading"><?php esc_html_e('SMS Notifications', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Send SMS notification and reminders to your customers.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-envelope"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Mailchimp Integration', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Automatically add customer email and personal info to your CRM or marketing platform like Mailchimp.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-bubble"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Chat with Customers', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Let your customers live chat with your or your agents directly from their person cabinet.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-bell1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Reminders', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Our powerful notification system lets you send SMS and email reminders for appointments.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-cart"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Taxes &amp; Fees', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Setup custom taxes or fees that are charged on top of appointment price.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-windows"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Bundles', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Sell a pack of appointments for a discounted price. Customers can schedule them anytime from their cabinet.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-upload1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Trigger Webhooks', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Send webhooks to external services like Zapier when a new booking is placed. Payload will include complete information about booking and customer.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-tag1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Coupons', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Create and manage discount codes with complex rules.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-play"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Workflows &amp; Automation', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Setup complex workflows to automate your booking business. Send webhooks, emails and sms when a specific event is triggered.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-settings"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Access Roles &amp; Permissions', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Advanced role management allows you to fine tune access permissions for your employees.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-windows"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Service Extras', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Make extra profit by upselling service extras in addition to a main service.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-clock"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Multi-durations', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Create multiple durations with different prices for the same service.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-map-marker"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Multiple Locations', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Setup multiple locations with different work hours, services and agents.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-users1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Multiple Agents', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Create multiple agents that offer different services. Each can be granted access to their personal dashboard.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-users1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Group Bookings', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Create services with minimum and maximum capacity, let customers book more than one attendee for a service.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-clock"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Timezone Picker', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e("Automatically detect customer's timezone and present calendar in their timezone.", 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-store"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Multiple Payment Processors', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Support for all major payment processors across the globe. Stripe, PayPal, Square, Mollie, RazorPay, MercadoPago, Flutterwave, Braintree, PayStack and more coming.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-bubble"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Whatsapp Notifications', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Notify customers via Whatsapp of an upcoming appointment or any other notification.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-book1"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Invoicing', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Let your customers view their invoices and make payments online.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-store"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Payment Links', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Send payment request links to customers that they can use to pay for their appointment.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-refresh"></i></div>
+    <div class="feature-heading"><?php esc_html_e('Refund Payments', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Refund payments directly from LatePoint dashboard.', 'latepoint'); ?></div>
+</div>
+<div class="os-premium-feature">
+    <div class="feature-icon"><i class="latepoint-icon latepoint-icon-cart"></i></div>
+    <div class="feature-heading"><?php esc_html_e('WooCommerce Integration', 'latepoint'); ?></div>
+    <div class="feature-description"><?php esc_html_e('Optional WooCommerce integration lets you utilize a power of WooCommerce payments with LatePoint bookings.', 'latepoint'); ?></div>
+</div>
+</div>
+</div>
+<?php
+    $license = OsLicenseHelper::get_license_info();
+
+    $post = array(
+        '_nonce'        => wp_create_nonce('addon_download'),
+        'license_key'   => $license['license_key'],
+        'domain'        => OsUtilHelper::get_site_url(),
+        'user_ip'       => OsUtilHelper::get_user_ip(),
+        'lp_version'    => '5',
+        'addon_name'    => 'latepoint-pro-features',
+    );
+    if( OsLicenseHelper::is_rtl_active() )
+        $request_download_url = wp_remote_post( OsLicenseHelper::get_remote_url("/addons/generate_download_url/latepoint-pro-features"), array('body' => $post, 'sslverify' => false));
+
+    if( ! empty( $request_download_url ) && !is_wp_error($request_download_url) || wp_remote_retrieve_response_code($request_download_url) === 200 ){
+        $dl_response = json_decode($request_download_url['body'], true);
+        $url = $dl_response['body']['download_url'];
+    }else{
+        $url = 'https://www.rtl-theme.com/license-activate-help/';
+    }
+?>
+<div class="latepoint-lightbox-footer">
+    <a href="<?php echo $url; ?>" target="_blank" class="latepoint-btn latepoint-btn-block upgrade-info-btn"><span><?php esc_html_e('Upgrade Now', 'latepoint'); ?></span><i class="latepoint-icon latepoint-icon-arrow-right"></i></a>
+</div>
